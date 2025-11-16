3a10801d72d4d50c94ea33e62bf9f2e050983045
Alemniscate-hub2 <freirealejo11c@gmail.com>
2025-11-15 20:53:19 -0500

    Add files via upload

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// función que muestra los valores
+void mostrarArreglo(int numeros[], int tamaño) {
+    cout << "Los valores almacenados en el arreglo son: " << endl;
+    for (int i = 0; i < tamaño; i++) {
+        cout << numeros[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    const int tamaño = 5; 
+    int numeros[tamaño];   
+
+    // Ingreso de los números
+    cout << "Ingrese cinco numeros enteros:" << endl;
+    for (int i = 0; i < tamaño; i++) {
+        cout << "Numero " << i + 1 << ": ";
+        cin >> numeros[i];
+    }
+    //función que muestra los valores
+    mostrarArreglo(numeros, tamaño);
+    return 0;
+}
